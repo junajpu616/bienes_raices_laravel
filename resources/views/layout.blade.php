a3bfdc3b7e059e6c2dedb9b9a97f73936e38cbf3
Junajpu <santiagojconos@gmail.com>
2025-09-07 20:54:56 -0600

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,9 @@
     <title>Bienes Raices Migración</title>
 
     <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 
 <body>
